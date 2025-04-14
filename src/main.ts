eb7a45a2abe62bd1f40657b1b9d95f959f2d66c9
Fernando Hidalgo Rosabal <fhidalgorosabal@gmail.com>
2025-04-14 10:38:37 -0300

    feature: Install Prisma

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { env } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Main');
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
   }));
   
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(env.port);
+  logger.log(`Application is running on port: ${env.port}`);
 }
 void bootstrap();
